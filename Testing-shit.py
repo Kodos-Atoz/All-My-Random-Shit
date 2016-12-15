b589775532dc9cabbe64cfa069f3f657374e4598
Kodos Macarthur <MyNameIsKodos@users.noreply.github.com>
2016-12-15 00:26:41 -0600

    Update Testing-shit.py

--- Testing-shit.py
+++ Testing-shit.py
@@ -1,9 +1,9 @@
-			elif texture.match(line) is not None:
+			elif sound.match(line) is not None:
 				#print(color.OKBLUE+line.strip('\n')+color.END)
-				uuid = line.replace("<TEXTURE:","")
-				uuid = uuid.replace(">","")
-				type = fetch(ASSET_TYPE % ('TEXTURE'))[0]
-				sql(INSERT_ASSET % (type,uuid.strip("\n")))
+				uuid = line.replace("<SOUND:","")
+                                uuid = uuid.replace(">","")
+				type = fetch(ASSET_TYPE % ('SOUND'))[0]
+				sql(INSERT_ASSET % (type,uuid.strip("\n"))) # SOUND
 				asset_id = fetch(ASSET_ID % (uuid.strip("\n")))[0]
-                                sql(INSERT_ASSET_LINE % (CID,asset_id,line_number))
-				line_number += 1
+				sql(INSERT_ASSET_LINE % (CID,asset_id,line_number))
+				line_number += 1 # Line Index + 1 IF its not the DESC line
